37d2bc665182b799d97e0c726503d95b538ee81f
伯翰 侯 <john860703@gmail.com>
2022-10-24 16:28:36 +0800

    加入專案檔案。

---
+++ Exec_while01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_while01
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//1+2+3+...+N <105 N的整數最大值
+			//使用公式  N(N+1)/2
+
+			int N = 1;
+			while (N * (N + 1) / 2 < 105)
+			{
+				N++;
+			}
+
+			//因為要求的解答為小於，因此N最後要再-1
+			Console.WriteLine(N-1);
+		}
+	}
+}
---
+++ Exec_while02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_while02
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//顯示所有可整除300的整數
+			int i = 2;
+			while (i <= 300)
+			{
+				if (300 % i == 0)
+				{
+					Console.WriteLine(i);
+				}
+
+				i++;
+			}
+		}
+	}
+}
